3f713a7266ebea1bc2a641338b58c5284c4d30bd
Riley <hidalgoriley@gmail.com>
2020-11-24 23:13:44 -0800

    Added defer to html code. added createcourseArray function to javascript code.

--- java/javascript.js
+++ java/javascript.js
@@ -1,31 +1,50 @@
-const course1 = {
-    code: "ACIT 1620",
-    name: 'Web Fundemental Technologies'
+// const course1 = {
+//     code: "ACIT 1620",
+//     name: 'Web Fundemental Technologies'
 
- };
-const course2 = {
-    code: "ACIT 1515",
-    name: 'Scripting for IT'
+//  };
+// const course2 = {
+//     code: "ACIT 1515",
+//     name: 'Scripting for IT'
      
- };
-const course3 = {
-    code: "MATH 1310",
-    name: 'Mathematics for IT'
+//  };
+// const course3 = {
+//     code: "MATH 1310",
+//     name: 'Mathematics for IT'
     
-};
-let courseList = [course1, course2, course3];
-let course = prompt("Enter four digit course number", );
-    while (course.length != 4) {
-        course = prompt("Please re-enter a correct course number" , );
+// };
+// let courseList = [course1, course2, course3];
+function createCourseArray(){
+    let courseList = [];
+    // let coursename = document.getElementsByClassName("subone")[0].innerHTML;
+    // let coursecode = document.getElementsByClassName('subthree')[0].innerHTML;
+    let coursename = document.querySelectorAll(".subone a")[0];
+    let coursecode = document.getElementsByClassName('subthree')[0].innerHTML;
+    coursename = (coursename.innerHTML)
+    let obj = {code:coursename, name:coursecode};
+    courseList.push(obj)
+    return courseList
+
 }
-for(let c of courseList){
-    if (course.includes("1515")) {
-        console.log(`Yes I am taking the course: ${course2.code} - ${course2.name}`);
-    } else if (course.includes("1620")) {
-        console.log(`Yes I am taking the course: ${course1.code} - ${course1.name}`);
-    } else if (course.includes("1310")) {
-        console.log(`Yes I am taking the course: ${course3.code} - ${course3.name}`);
-    } else { (courseList[3]={course})
-        console.log('Your course has been added to the list')
-    }
-}
+console.log(createCourseArray())
+// let course = prompt("Enter four digit course number", );
+//     while (course.length != 4 || (isNaN(course))) {
+//         course = prompt("Please re-enter a correct course number" , );
+// }
+// let courseExist = false;
+
+// for(let courseitem of courseList){
+//    if (courseitem.code.includes(course)){
+//     console.log(`Yes I am taking the course: ${courseitem.code} - ${courseitem.name}`);
+//     courseExist = true;
+//     break;
+//    }
+// }
+// if (courseExist === false){
+//     let obj = {code:course, name:null};
+//     courseList.push(obj);
+//     console.log(`new course added`);
+//     console.log(courseList);
+
+
+// }
